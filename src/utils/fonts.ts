28295ed165b40d85d16670d266a04d9916643d4f
Rehan <rehanadipurwana@gmail.com>
2024-03-19 12:55:19 +0700

    build: setup fonts

---
+++ src/utils/fonts.ts
@@ -0,0 +1,11 @@
+import { Open_Sans, Sarpanch } from "next/font/google";
+
+export const openSans = Open_Sans({ 
+  subsets: ['latin'], 
+  weight: ['400', '500', '600', '700', '800'] 
+});
+
+export const sarpanch = Sarpanch({ 
+  subsets: ['latin'], 
+  weight: ['400', '500', '600', '700', '800', '900'] 
+});
